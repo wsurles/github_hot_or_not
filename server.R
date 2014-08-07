e89d7701180c9e12c6fe48911cf7d6f59fec1314
William Surles <williamsurles@gmail.com>
2014-08-07 12:13:32 -0600

    clean up and adding comments

--- server.R
+++ server.R
@@ -1,15 +1,10 @@
 ##| server.R
 
-##| For running locally
-# library(shiny)
-# setwd("~/Dev/non_work_projects")
-# runApp("app_github_hot")
-
 options(RCHART_WIDTH = 1000)
  
 shinyServer(function(input, output, session) {
-
-  ##| Source Files
+  
+  ##| Load ui and server functions
   source('lib/ui_tabs.R', local = T)
   source('lib/server_github.R', local = T)
   
